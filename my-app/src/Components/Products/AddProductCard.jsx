0620b48e30dcb5269eca610cb53981eb9e336e29
AKSHAYMULANI <m73592r@gmail.com>
2022-07-21 19:39:14 +0530

    day-3 task api and get request completed

--- my-app/src/Components/Products/AddProductCard.jsx
+++ my-app/src/Components/Products/AddProductCard.jsx
@@ -1,14 +1,15 @@
 import { Image, Stack, Text } from "@chakra-ui/react";
 
 
-function AddProductCard({imgUrl, Title, Colors, price }){
+function AddProductCard({imgUrl, Title, Colors, price, desc }){
     return (
         <div>
-            <Stack w={'100%'}>
+            <Stack w={'100%'} textAlign='left' p={'5px'}>
                 <Image w={'100%'} src={imgUrl} />
-                <Text>{Title}</Text>
-                <Text>Colors:{Colors}</Text>
-                <Text>${price}</Text>
+                <Text>  {Title}</Text>
+                <Text>  {desc}</Text>
+                <Text>  Colors:{Colors}</Text>
+                <Text>  ${price}</Text>
 
             </Stack>
         </div>
