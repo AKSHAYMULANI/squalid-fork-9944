fe6099b6fa40d167ef6669bbe8ab6a0335fc77f3
AKSHAYMULANI <m73592r@gmail.com>
2022-07-23 19:38:11 +0530

    filter & sort

--- my-app/src/Components/Products/Showproduct.jsx
+++ my-app/src/Components/Products/Showproduct.jsx
@@ -1,28 +1,88 @@
-import { Flex, Grid } from "@chakra-ui/react"
+import { Flex, Grid,Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react"
 import { useEffect } from "react"
 import { useState } from "react"
 import MainHeader from "../MainHeader"
 import AddProductCard from "./AddProductCard"
-import { getProducts } from "./productApi"
+import { getPricefilterSort, getProducts, getPriceSort } from "./productApi"
 import ProductFilter from "./productFilter"
+// import SortProduct from "./SortProduct";
 
 
 function ShowProducts({tag}){
 
     const [data, setData] = useState([])
+    const [value, setValue] = useState(-1)
+    const [type, setType] = useState("")
+    const [priceSort, setPriceSort] = useState("")
+
+    function Handlesort(valueofprice){
+        setPriceSort(valueofprice)
+        if(value==='-1' && priceSort!==""){
+            getPriceSort(type,priceSort).then((res)=>{
+                setData((res.data))
+        })
+        }
+    }
 
     useEffect(()=>{
+        setValue("-1")
         getProducts(tag).then((res)=>{
         setData(res.data)
+        setType(tag)
+        setPriceSort("")
+        })
+        
+        
+    },[tag]) 
+
+    useEffect(()=>{
+   
+  
+
+    if(value==='1'){
+        getPricefilterSort(type,1,10,priceSort).then((res)=>{
+        setData((res.data))
     })
-    }, [tag])
+    }
     
+    if(value==="10"){
+        getPricefilterSort(type,10,20,priceSort).then((res)=>{
+            setData(res.data)
+        })
+        
+    }
+    if(value==="20"){
+        getPricefilterSort(type,20,30,priceSort).then((res)=>{
+            setData(res.data)
+        })
+    }
+    if(value==="30"){
+        getPricefilterSort(type,30,400,priceSort).then((res)=>{
+            setData(res.data)
+        })
+    }
+    
+    }, [value,type,priceSort])
+    
+    console.log(priceSort, value, data)
 
     return (
         <div>
             <MainHeader />
+            <div>
+            <Menu closeOnSelect={true}>
+            <MenuButton as={Button} colorScheme='blue'>
+            Sort by: Price 
+            </MenuButton>
+            <MenuList minWidth='240px'>
+            <MenuItem onClick={()=>Handlesort('')}>Relevance(default)</MenuItem>
+            <MenuItem  onClick={()=>Handlesort('asc')} >Low to High</MenuItem>
+            <MenuItem onClick={()=>Handlesort('desending')}>High to Low</MenuItem>
+            </MenuList>
+            </Menu>
+        </div>
             <Flex m='auto' w={'90%'} justify="space-evenly">
-                <ProductFilter ProTag={tag} />
+                <ProductFilter ProTag={tag} value={value} setValue={setValue} />
                 <Grid templateColumns='repeat(4, 1fr)' gap={5} m='auto' w={'80%'}>
                     {data.map((item) => (
                         <AddProductCard
--- my-app/src/Components/Products/productApi.js
+++ my-app/src/Components/Products/productApi.js
@@ -4,6 +4,20 @@ export function getProducts(product_type){
     return axios.get(`https://sephora-unit-4.herokuapp.com/posts?type=${product_type} `)
 }
 
+export function getPriceFiltered(product_type,min,max){
+  return axios.get(`https://sephora-unit-4.herokuapp.com/posts?type=${product_type}&price_gte=${min}&price_lte=${max}
+  `)
+}
+
+export function getPricefilterSort(product_type,min,max,sort){
+  return axios.get(`https://sephora-unit-4.herokuapp.com/posts?type=${product_type}&price_gte=${min}&price_lte=${max}&_sort=price&order=${sort}
+  `)
+}
+export function getPriceSort(product_type,sort){
+  return axios.get(`https://sephora-unit-4.herokuapp.com/posts?type=${product_type}&_sort=price&order=${sort}
+  `)
+}
+
 export function addProductApi({ type,title,desc,color,price,imgUrl }) {
     return axios({
       url: `https://sephora-unit-4.herokuapp.com/posts/`,
